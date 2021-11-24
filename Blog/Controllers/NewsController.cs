5e3b9200fdc96a84381dbd6c60f7e1519bad883d
IlyaI <IlyaI@DESKTOP-6HGUK6R>
2021-11-24 19:43:18 +0300

    223222323

---
+++ Blog/Controllers/NewsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Blog.Domain;
+
+namespace MyCompany.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public NewsController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Index(Guid id)
+        {
+            if (id != default)
+            {
+                return View("Show", dataManager.NewsItems.GetNewsItemById(id));
+            }
+
+            ViewBag.TextField = dataManager.TextFields.GetTextFieldByCodeWord("PageNews");
+            return View(dataManager.NewsItems.GetNewsItems());
+        }
+    }
+}
